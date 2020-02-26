ffa40227d5fbbdcc3eecbb0247cfa2b795c1f883
he.fanghui <he.fanghui@imprexion.com.cn>
2020-02-26 08:51:47 +0800

    fix: fix compile errors

---
+++ Test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Test
+//
+//  Created by 002 on 2020/2/26.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
